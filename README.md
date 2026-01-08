dd18b47bdc4dae084f1f9445eabb61ca47ef6e0d
korges <korgesik@gmail.com>
2026-01-08 17:29:35 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+#About
+
+1. Create a basic Account Abstraction on Ethereum
+2. Create a basic Account Abstraction on ZkSync
+3. Deploy, and send some userOp / transaction through them
+
+EIP-4337
+ECDSA
---
+++ test/MinimalAccountTest.t.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.30;
+
+import {Test} from "forge-std/Test.sol";
+import {MinimalAccount} from "src/ethereum/MinimalAccount.sol";
+import {DeployMinimal} from "script/DeployMinimal.s.sol";
+import {HelperConfig} from "script/HelperConfig.s.sol";
+
+contract MinimalAccountTest is Test {
+
+}
